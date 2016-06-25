c3fe402e56f198994dfb6d6db09a38fe731d63f3
Laure Daumal <daumal_l@epita.fr>
2016-06-25 17:34:36 +0200

    add Listeners on buttons

--- app/src/main/java/epita/gamebox/HangmanFragment.java
+++ app/src/main/java/epita/gamebox/HangmanFragment.java
@@ -9,11 +9,16 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 /**
  * Created by Jérémy on 11/06/2016.
  */
-public class HangmanFragment extends Fragment {
+public class HangmanFragment extends Fragment implements View.OnClickListener {
+
+    private Button[][] alphabet;
 
     public HangmanFragment() {}
 
@@ -22,6 +27,21 @@ public class HangmanFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
         final View view = inflater.inflate(R.layout.hangmanfragment, container, false);
+
+        // Get elements
+        GridLayout grid = (GridLayout) view.findViewById(R.id.LetterGrid);
+        alphabet = new Button[5][5];
+        int i = 0;
+        int j = 0;
+        for (int k = 0; k < grid.getChildCount(); ++k){
+            if (k % 5 == 0 && k != 0) {
+                ++i;
+                j = 0;
+            }
+            alphabet[i][j] = (Button) grid.getChildAt(k);
+            alphabet[i][j].setOnClickListener(this);
+            ++j;
+        }
 
         return view;
     }
@@ -42,6 +62,17 @@ public class HangmanFragment extends Fragment {
                 break;
         }
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        for (int i = 0; i < 5; ++i)
+            for (int j = 0; j < 5; ++j)
+                if (v == alphabet[i][j]) {
+                    Toast.makeText(getContext(), "letter " + alphabet[i][j].getText() + " choosen",
+                            Toast.LENGTH_SHORT).show();
+                    alphabet[i][j].setEnabled(false);
+                }
     }
 
     public interface hangmanAction {
